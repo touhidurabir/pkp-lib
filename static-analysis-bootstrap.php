bad8da1ae32c3016e352e1bbe0cd005df97cc7ad
Touhidur Rahman <abir.ruet@gmail.com>
2022-06-10 17:57:06 +0600

    static analysis bootstrap update

--- static-analysis-bootstrap.php
+++ static-analysis-bootstrap.php
@@ -1,14 +1,27 @@
 <?php
 
 // Load Composer autoloader
-require_once 'lib/vendor/autoload.php';
+require_once './lib/vendor/autoload.php';
+
+// Define the starting index.php file path
+define('INDEX_FILE_LOCATION', dirname(__FILE__, 3) . '/index.php');
+
+define('BASE_SYS_DIR', dirname(INDEX_FILE_LOCATION));
+chdir(BASE_SYS_DIR);
 
 // System-wide functions
-require_once './includes/functions.inc.php';
+require_once 'includes/functions.inc.php';
 
 // Register custom autoloader functions for namespaces
 spl_autoload_register(function ($class) {
     $prefix = 'PKP\\';
-    $rootPath = 'classes';
+    $rootPath = BASE_SYS_DIR . '/lib/pkp/classes';
     customAutoload($rootPath, $prefix, $class);
 });
+spl_autoload_register(function ($class) {
+    $prefix = 'APP\\';
+    $rootPath = BASE_SYS_DIR . '/classes';
+    customAutoload($rootPath, $prefix, $class);
+});
+
+return new \APP\core\Application();
