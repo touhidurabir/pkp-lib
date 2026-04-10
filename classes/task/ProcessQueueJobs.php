0145043f3616f624d2a4a49e570358f6877867bf
Touhidur Rahman <abir.ruet@gmail.com>
2026-04-10 07:14:27 +0600

    pkp/pkp-lib#12532 job runner flush buffer and cross request locking (#12535)
    * pkp/pkp-lib#12532 job runner flush buffer and cross request locking
    
    * pkp/pkp-lib#12532 add missing nuffer flush for schedule tasks running in web mode

--- classes/task/ProcessQueueJobs.php
+++ classes/task/ProcessQueueJobs.php
@@ -3,13 +3,11 @@
 /**
  * @file classes/task/ProcessQueueJobs.php
  *
- * Copyright (c) 2022 Simon Fraser University
- * Copyright (c) 2022 John Willinsky
+ * Copyright (c) 2014-2026 Simon Fraser University
+ * Copyright (c) 2000-2026 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class ProcessQueueJobs
- *
- * @ingroup tasks
  *
  * @brief Class to process queue jobs via the scheduler task
  */
@@ -18,7 +16,6 @@ namespace PKP\task;
 
 use PKP\config\Config;
 use PKP\core\PKPContainer;
-use PKP\queue\JobRunner;
 use PKP\scheduledTask\ScheduledTask;
 
 class ProcessQueueJobs extends ScheduledTask
@@ -55,7 +52,7 @@ class ProcessQueueJobs extends ScheduledTask
         // will process a limited number of jobs at a single time
         if (PKPContainer::getInstance()->runningInConsole()) {
             $maxJobCountToProcess = abs(Config::getVar('queues', 'job_runner_max_jobs', 30));
-            
+
             while ($jobBuilder->count() && $maxJobCountToProcess) {
                 $jobQueue->runJobInQueue();
                 --$maxJobCountToProcess;
@@ -69,8 +66,14 @@ class ProcessQueueJobs extends ScheduledTask
             return true;
         }
 
-        // Executes a limited number of jobs when processing a via web request mode
-        (new JobRunner($jobQueue))
+        // Will never run the job runner in CLI mode
+        if (PKPContainer::getInstance()->runningInConsole()) {
+            return true;
+        }
+
+        // Executes a limited number of jobs when processing a request
+        $jobRunner = app('jobRunner'); /** @var \PKP\queue\JobRunner $jobRunner */
+        $jobRunner
             ->withMaxExecutionTimeConstrain()
             ->withMaxJobsConstrain()
             ->withMaxMemoryConstrain()
